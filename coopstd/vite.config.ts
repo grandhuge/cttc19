e60cee937e411b83ac0a18aa7c73b0f3edf104e2
grandhuge <narongphisit@gmail.com>
2025-06-29 07:29:59 +0700

    Update vite.config.ts

--- coopstd/vite.config.ts
+++ coopstd/vite.config.ts
@@ -6,7 +6,7 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
-    host: "::",
+    host: "/cttc19/coopstd/",
     port: 8080,
   },
   plugins: [
